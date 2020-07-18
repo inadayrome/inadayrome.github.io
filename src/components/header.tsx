cb664de2fb2a8429b8dd69d5925cbace9c9f6644
inadayrome <1994leewj@gmail.com>
2020-07-18 13:16:59 +0800

    update: Added link to Github repository through GithubIcon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,15 @@
 import { Link } from 'gatsby';
 import React from 'react';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import { IconButton } from '@material-ui/core';
+import { GITHUB_URL } from '../constants';
 
 interface HeaderProps {
   siteTitle: string;
 }
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
     root: {
       marginBottom: '1.45rem',
@@ -16,6 +19,9 @@ const useStyles = makeStyles((theme: Theme) =>
       maxWidth: '960px',
       padding: '1.45rem 1.0875rem',
       borderBottom: '1px solid #eee',
+      display: 'grid',
+      justifyContent: 'space-between',
+      gridTemplateColumns: 'auto auto',
     },
     homePageLink: {
       color: '#777',
@@ -30,13 +36,18 @@ const Header: React.FC<HeaderProps> = props => {
   const classes = useStyles();
 
   return (
-    <header style={{ marginBottom: '1.45rem' }}>
+    <header className={classes.root}>
       <div className={classes.headerDiv}>
-        <h5 style={{ margin: 0 }}>
+        <h5 style={{ margin: 0, alignSelf: 'center' }}>
           <Link to="/" className={classes.homePageLink}>
             {siteTitle}
           </Link>
         </h5>
+        <a href={GITHUB_URL}>
+          <IconButton title="View Github Repository" color="primary">
+            <GitHubIcon />
+          </IconButton>
+        </a>
       </div>
     </header>
   );
---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const GITHUB_URL = 'https://github.com/inadayrome/inadayrome.github.io';
