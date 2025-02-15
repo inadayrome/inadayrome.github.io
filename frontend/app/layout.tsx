2f73f4b3428f513408e3285a95be447ced233448
inadayrome <1994leewj@gmail.com>
2025-02-15 11:16:35 +0800

    add base outline

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,10 +4,11 @@ import './globals.css';
 import {
   NavigationMenu,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
 } from '@/components/ui/navigation-menu';
 import Link from 'next/link';
+import { GITHUB_URL, LINKEDIN_URL } from '@/lib/utils';
+import { GitHubLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -33,14 +34,14 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased font-[family-name:var(--font-geist-mono)]`}
+        className={`${geistSans.variable} ${geistMono.variable} min-h-screen antialiased font-[family-name:var(--font-geist-mono)]`}
       >
-        <div className='max-w-full flex flex-1 justify-between'>
+        <div className='max-w-full flex flex-1 justify-between p-4 border-b border-neutral-700'>
           <NavigationMenu>
             <NavigationMenuList>
               <NavigationMenuItem>
                 <Link href='/' passHref>
-                  <NavigationMenuLink>inadayrome</NavigationMenuLink>
+                  inadayrome
                 </Link>
               </NavigationMenuItem>
             </NavigationMenuList>
@@ -49,13 +50,23 @@ export default function RootLayout({
           <NavigationMenu>
             <NavigationMenuList className='gap-4'>
               <NavigationMenuItem>
-                <Link href='/Projects' passHref>
-                  <NavigationMenuLink>Projects</NavigationMenuLink>
+                <Link href='/projects' passHref>
+                  Projects
                 </Link>
               </NavigationMenuItem>
               <NavigationMenuItem>
-                <Link href='/About' passHref>
-                  <NavigationMenuLink>About</NavigationMenuLink>
+                <Link href='/about' passHref>
+                  About
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href={LINKEDIN_URL}>
+                  <LinkedInLogoIcon />
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href={GITHUB_URL}>
+                  <GitHubLogoIcon />
                 </Link>
               </NavigationMenuItem>
             </NavigationMenuList>
--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const GITHUB_URL = 'https://github.com/inadayrome/inadayrome.github.io';
+
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/wenjunlee1994';
